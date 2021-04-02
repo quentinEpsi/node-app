30d0f83524f9b942a3437f0406c2aa9df0f123e7
QuentinB <quentin.bernard@hotmail.fr>
2021-04-02 14:51:38 +0200

    change sending

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();
 app.get('/', function (req, res) {
- res.send('demo');
+ res.send('mspr demo');
 });//listen to port 3000 by default
 
 app.listen(process.env.PORT || 3200); 
